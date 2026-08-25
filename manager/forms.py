d6dd1ceaba1498b30576ab85cc52b54f6c887a98
Olha Kuzmyn <olya.kuzmyn@gmail.com>
2026-08-25 14:17:00 +0300

    removed wrong import

--- manager/forms.py
+++ manager/forms.py
@@ -1,4 +1,3 @@
-from black.brackets import Priority
 from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm, PasswordChangeForm
