4dc72224da1f51e279d633f11d88a7aedcefc0bc
Olha Kuzmyn <olya.kuzmyn@gmail.com>
2026-09-01 17:39:36 +0300

    fixed add_user_to_manager_groups to have is_manager as true by default

--- apps/worker/signals.py
+++ apps/worker/signals.py
@@ -9,3 +9,4 @@ def add_user_to_manager_groups(sender, instance, created, **kwargs):
     """function to add superuser to manager group by default"""
     if instance.is_superuser:
         instance.groups.add(Group.objects.get(name="Manager"))
+        instance.is_manager = True
