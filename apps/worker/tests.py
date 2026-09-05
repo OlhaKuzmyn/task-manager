13101a80a4886b18d8ca10132d5955cc301797af
Olha Kuzmyn <olya.kuzmyn@gmail.com>
2026-09-05 23:04:23 +0300

    added test for worker creation functionality

--- apps/worker/tests.py
+++ apps/worker/tests.py
@@ -1,3 +1,87 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group, Permission
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+WORKER_CREATE_URL = reverse("worker:worker-create")
+
+
+class TestWorkerManagerView(TestCase):
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username="manager_user",
+            password="test1234"
+        )
+        self.manager_group = Group.objects.get(name="Manager")
+        self.manager_group.permissions.set(Permission.objects.filter(codename__in=["add_worker", "change_worker"]))
+        self.user.groups.add(self.manager_group)
+        self.user.is_manager = True
+        self.user.save()
+        self.client.force_login(self.user)
+
+        self.form_data = {
+            "username": "test_user",
+            "password1": "us3r12t@st@new",
+            "password2": "us3r12t@st@new",
+            "first_name": "Test First",
+            "last_name": "Test Last",
+        }
+
+        self.response = self.client.post(WORKER_CREATE_URL, data=self.form_data)
+
+    def test_worker_creation(self):
+        self.assertEqual(self.response.status_code, 302)
+        new_user = get_user_model().objects.get(
+            username=self.form_data["username"]
+        )
+        self.assertEqual(
+            self.form_data["first_name"], new_user.first_name
+        )
+        self.assertEqual(
+            self.form_data["last_name"], new_user.last_name
+        )
+
+    def test_worker_update_manager(self):
+        new_upd_user = get_user_model().objects.get(
+            username=self.form_data["username"]
+        )
+        WORKER_DETAIL_URL = reverse("worker:worker-detail", kwargs={"pk": new_upd_user.id})
+        upd_response = self.client.post(WORKER_DETAIL_URL)
+        self.assertEqual(upd_response.status_code, 302)
+
+        new_upd_user.refresh_from_db()
+
+        self.assertEqual(
+            True, new_upd_user.groups.filter(name="Manager").exists() and new_upd_user.is_manager
+        )
+
+    def test_superuser_is_manager(self):
+        new_super_user = get_user_model().objects.create_superuser(
+            username="superuser_user",
+            email="test@test.com",
+            password="us3r12t@st@new"
+
+        )
+        self.assertEqual(
+            True, new_super_user.groups.filter(name="Manager").exists() and new_super_user.is_manager
+        )
+
+
+class TestWorkerNonManagerView(TestCase):
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username="worker_test_user",
+            password="test1234"
+        )
+        self.client.force_login(self.user)
+
+    def test_worker_creation_not_possible(self):
+        form_data = {
+            "username": "test_user",
+            "password1": "us3r12t@st@new",
+            "password2": "us3r12t@st@new",
+            "first_name": "Test First",
+            "last_name": "Test Last",
+        }
+        response = self.client.post(WORKER_CREATE_URL, data=form_data)
+        self.assertEqual(response.status_code, 403)
