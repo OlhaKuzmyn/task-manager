79a8278c78cef7985d69beb07be647474601472f
Olha Kuzmyn <olya.kuzmyn@gmail.com>
2026-09-05 18:15:29 +0300

    added allowed hosts to settings.py

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True if os.environ.get('DEBUG') == "True" else False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "http://localhost:8000"
+]
 
 
 # Application definition
