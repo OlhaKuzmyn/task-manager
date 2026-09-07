72ea0fafb73280b1cb88fdbc4bc7c117bb13e8f9
Olha Kuzmyn <olya.kuzmyn@gmail.com>
2026-09-07 22:38:04 +0300

    added test for position creation not working for regular users

--- apps/position/tests.py
+++ apps/position/tests.py
@@ -1,3 +1,25 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+POSITION_CREATION_URL = reverse("position:position-create")
+
+
+class TestPositionUserNonManager(TestCase):
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username="worker_test_user",
+            password="test1234"
+        )
+        self.client.force_login(self.user)
+
+    def test_position_creation_not_possible(self):
+        form_data = {
+            "name": "Test Position",
+        }
+        response = self.client.post(
+            POSITION_CREATION_URL,
+            data=form_data
+        )
+        self.assertEqual(response.status_code, 403)
