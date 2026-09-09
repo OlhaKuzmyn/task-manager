16f425e88ea4d9705f759ab5c2a16d956b740cf9
Olha Kuzmyn <olya.kuzmyn@gmail.com>
2026-09-09 17:30:33 +0300

    added build.sh and render settings to project to prepare for deployment to Render

--- task_manager/settings/prod.py
+++ task_manager/settings/prod.py
@@ -4,10 +4,11 @@ from .base import *
 DEBUG = False
 # DEBUG = True if os.environ.get("DEBUG") == "True" else False
 
-ALLOWED_HOSTS = [
-    "127.0.0.1",
-    "localhost",
-]
+ALLOWED_HOSTS = []
+
+RENDER_EXTERNAL_HOSTNAME = os.environ.get("RENDER_EXTERNAL_HOSTNAME")
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 
 # Database
